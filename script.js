e9cd6ec76af92e6ae69d18eb568b8b7b3b084441
LaLondinense <lalondinenseingles@gmail.com>
2025-05-01 17:00:05 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function flipCard(card) {
+  card.classList.toggle('flipped');
+}
+
+function playAudio(filename) {
+  const audio = new Audio('audio/' + filename);
+  audio.play();
+body.appendParagraph("----- Card " + i + " -----");
+}
